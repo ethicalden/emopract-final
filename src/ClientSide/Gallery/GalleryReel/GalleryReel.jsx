06d98f1f889c54d97181c21a127f96a1336995f4
dmemopract <dmemopract@gmail.com>
2024-10-20 23:20:30 -0600

    auto play added

--- src/ClientSide/Gallery/GalleryReel/GalleryReel.jsx
+++ src/ClientSide/Gallery/GalleryReel/GalleryReel.jsx
@@ -8,6 +8,9 @@ const GalleryReel = () => {
           className="h-full w-full object-cover rounded-[15px]"
           src="/Images/GalleryReel.mp4" // Accessing directly from public folder
           controls
+          autoPlay
+          loop // Optional: To make the video repeat
+          muted // Optional: Mute the video (often needed for autoplay to work without user interaction)
           height={800}
           alt="Gallery Reel"
         />
